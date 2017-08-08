2e5e70903b69b5406b4621114c022053cebca04f
Uche Ndububa <n.diamond.705@gmail.com>
2017-08-08 23:07:03 +0100

    updated master blade blue print

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,12 +1,22 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{$title or 'blank title'}}</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
 
+    @include('utils.links')
+    @yield('custom-links')
+</head>
 
-@section('custom-links')
-@endsection
+<body>
+@include('utils.header')
 
-@section('content')
-@endsection
+@yield('content')
 
-@section('custom-scripts')
-@endsection
+@yield('custom-scripts')
+</body>
+</html>
 
