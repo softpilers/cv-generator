794bc290f1410d574d105b409811cf8bcc5e06c7
Uche Ndububa <n.diamond.705@gmail.com>
2017-08-18 18:10:52 +0100

    updated social auth controller

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Social;
 use App\Models\User;
 use App\Models\Profile;
-use App\Traits\ActivationTrait;
-use App\Traits\CaptureIpTrait;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Log;
-use Laravel\Socialite\Facades\Socialite;
-use jeremykenedy\LaravelRoles\Models\Permission;
-use jeremykenedy\LaravelRoles\Models\Role;
 
 class SocialAuthController extends Controller
 {
